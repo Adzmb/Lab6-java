2f4d92b00d1a37bef4d17cc0c7c4711b919d32a3
Ade Adelokiki <adz@mac.home>
2026-03-19 14:53:32 +0000

    Changes I'm making to the main branch

--- Lab6.java
+++ Lab6.java
@@ -2,5 +2,6 @@ class Lab6{
 
 public static void main(String[] args){
 	System.out.println("Hello World");
+	System.out.println("This is the second change I'm making to the file however this is in my main branch and not in my other branch then I will attempt to merge them and see what happens probably a confilct will occur");
 	}
 }
